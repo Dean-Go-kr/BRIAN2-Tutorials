d964a03700c9242a6c903fb85edd018e0bdb848e
Dean-Go-Kr <dean.go.kr@gmail.com>
2022-02-18 17:16:48 +0900

    Part3-end of firing rates

--- Part3_Simulations.py
+++ Part3_Simulations.py
@@ -21,11 +21,11 @@ MP = SpikeMonitor(P)
 
 # We use a Network object because later on we don't
 # want to include these objects
-
 net = Network(P, MP)
 net.run(1*second)
 
-# And keep a coppy of those spikes
+# And keep a copy of those spikes
+# i: indices / t: spike times
 spikes_i = MP.i
 spikes_t = MP.t
 
@@ -53,6 +53,38 @@ for tau in tau_range:
     net.run(1*second)
     output_rates.append(M.num_spikes/second)
 
+plot(tau_range/ms, output_rates)
+xlabel(r'$\tau$ (ms)')
+ylabel('Firing rate (sp/s)')
+
+#%%
+### More efficient way ###
+
+start_scope()
+num_inputs = 100
+input_rate = 10*Hz
+weight = 0.1
+tau_range = linspace(1, 10, 30)*ms
+num_tau = len(tau_range)
+P = PoissonGroup(num_inputs, rates=input_rate)
+
+# We make tau a parameter of the group
+eqs = '''
+dv/dt = -v/tau : 1
+tau : second
+'''
+
+# And we have num_tau output neurons, each with a different tau
+G = NeuronGroup(num_tau, eqs, threshold='v>1', reset='v=0', method='exact')
+G.tau = tau_range
+S = Synapses(P, G, on_pre='v += weight')
+S.connect()
+M = SpikeMonitor(G)
+
+# Now we can just run once with no loop
+run(1*second)
+output_rates = M.count/second #firing rate is count/duration
+print(M.v)
 plot(tau_range/ms, output_rates)
 xlabel(r'$\tau$ (ms)')
 ylabel('Firing rate (sp/s)')
